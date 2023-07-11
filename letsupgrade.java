960e3cc52888244ad143b6246196db5ed891147f
Parishad Karki <karkiparishad23@gmail.com>
2023-07-11 23:49:41 +0530

    Create letsupgrade.java

---
+++ letsupgrade.java
@@ -0,0 +1,7 @@
+
+numbers = []
+for i in range(5):
+    number = int(input("Enter an integer: "))
+    numbers.append(number)
+sum_of_numbers = sum(numbers)
+print("The sum of the numbers is:", sum_of_numbers)
